5c837cb48c93817d89fa73243c8371f25b3f7c38
jbarrosgarcia <jbarrosgarcia@danielcastelao.org>
2018-11-21 12:01:02 +0100

    first commit

---
+++ Scripts/ControlJugador.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlJugador : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        rb=GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 mover = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(mover*speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
